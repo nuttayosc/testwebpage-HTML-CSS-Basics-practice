c2f483514a3142a8c722bb7c479de2681addf9be
Nuttayos Charukulvanich <122503568+nuttayosc@users.noreply.github.com>
2023-02-11 00:08:10 +0700

    JS Datatypes

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
-document.write("testing JS");
+var x = "5" + 2 + 3; //will print 523, store as string
+
+var y = 2 + 3 + "5"; //will print 55, store as string
+
+var z = ["John", "Taobin", "Trivago"]; //Array Z
+
+//Object person
+var person = {firstName: "John", 
+            lastName: "Taobin", 
+            Hotel: "Trivago", 
+            fullName : function() {
+                return this.firstName+" "+this.lastName;
+                }
+            }; 
+
+// document.write(x+typeof(x));
+document.write(person.fullName);
+document.getElementById("demo").innerHTML = Name.fullName;
